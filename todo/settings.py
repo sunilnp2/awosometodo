3403869ab884c46bf7e8fd839ffd156526bf20f6
Sunil Nepali <gorkhalisunil2016@gmail.com>
2022-06-23 15:57:17 +0545

    requirement txt

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$j#f)nxq&ncr+%$#an@z-+289^v4^utuda%&+u9#b67k66i&my'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
