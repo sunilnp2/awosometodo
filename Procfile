8ee34d19a926c57516c432b8821846d5f6cf02ae
Sunil Nepali <gorkhalisunil2016@gmail.com>
2022-06-23 16:08:59 +0545

    debug

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn razor.wsgi --log-file
+web: gunicorn todo.wsgi --log-file
--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$j#f)nxq&ncr+%$#an@z-+289^v4^utuda%&+u9#b67k66i&my'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["awosometodo.herokuapp.com", "localhost"]
 
 
 # Application definition
